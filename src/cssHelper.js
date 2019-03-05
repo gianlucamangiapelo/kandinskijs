8604df314d9ae2aaac26fb12f09d28361ff9a4a3
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2019-03-05 09:44:07 +0100

    Add first implementation of cssHelper.js

---
+++ src/cssHelper.js
@@ -0,0 +1,13 @@
+module.exports = {
+	hexTorgb: function (hex) {
+		const hs = hex[0] === '#' ? hex.slice(1) : hex;
+		return [
+			parseInt(hs[0] + hs[1], 16),
+			parseInt(hs[2] + hs[3], 16),
+			parseInt(hs[4] + hs[5], 16)
+		];
+	},
+	rgbTohex: function (rgb) {
+		rgb.reduce((hex, c) => hex + padHex(c.toString(16)), '#');
+	}
+};
