e26f5e9670138ac65d5e0edfd3fe9f86b164074e
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2019-03-16 13:04:38 +0100

    Update indentations

--- demo/cssModuleTest.js
+++ demo/cssModuleTest.js
@@ -2,83 +2,83 @@ var expect = require("chai").expect;
 const pup = require("../src/pupPlugin");
 
 describe("h2 css test", function () {
-	let page;
+  let page;
 
-	const url = "http://localhost:4000";
-	const cssPath = "demo/base.css";
+  const url = "http://localhost:4000";
+  const cssPath = "demo/base.css";
 
-	before(async function () {
-		await pup.init(url, cssPath);
-	});
+  before(async function () {
+    await pup.init(url, cssPath);
+  });
 
-	after(async function () {
-		await pup.destroy();
-	});
+  after(async function () {
+    await pup.destroy();
+  });
 
-	context("mobile viewport", function () {
-		before(async function () {
-			this.timeout(1000000);
-			page = await pup.getPage({ width: 320, height: 1 });
-		});
-		after(async function () {
-			await page.close();
-		});
-	});
+  context("mobile viewport", function () {
+    before(async function () {
+      this.timeout(1000000);
+      page = await pup.getPage({ width: 320, height: 1 });
+    });
+    after(async function () {
+      await page.close();
+    });
+  });
 
-	context("tablet viewport", function () {
-		before(async function () {
-			this.timeout(1000000);
-			page = await pup.getPage({ width: 768, height: 1 });
-		});
-		after(async function () {
-			await page.close();
-		});
+  context("tablet viewport", function () {
+    before(async function () {
+      this.timeout(1000000);
+      page = await pup.getPage({ width: 768, height: 1 });
+    });
+    after(async function () {
+      await page.close();
+    });
 
-		it("h2 should have a display: flex", async function () {
-			const display = await pup.getCSSProperty(page, "h2", "display");
+    it("h2 should have a display: flex", async function () {
+      const display = await pup.getCSSProperty(page, "h2", "display");
 
-			expect(display).to.eql("flex");
-		});
-	});
+      expect(display).to.eql("flex");
+    });
+  });
 
-	context("desktop viewport", function () {
-		before(async function () {
-			this.timeout(1000000);
-			page = await pup.getPage({ width: 1024, height: 1 });
-			//await page.screenshot({ path: 'page.png' })
-		});
-		after(async function () {
-			await page.close();
-		});
+  context("desktop viewport", function () {
+    before(async function () {
+      this.timeout(1000000);
+      page = await pup.getPage({ width: 1024, height: 1 });
+      //await page.screenshot({ path: 'page.png' })
+    });
+    after(async function () {
+      await page.close();
+    });
 
-		it("h2 should have a display: block", async function () {
-			const display = await pup.getCSSProperty(page, "h2", "display");
+    it("h2 should have a display: block", async function () {
+      const display = await pup.getCSSProperty(page, "h2", "display");
 
-			expect(display).to.eql("block");
-		});
+      expect(display).to.eql("block");
+    });
 
-		it("should have a heading color", async function () {
-			const color = await pup.getCSSProperty(page, "h2", "color");
+    it("should have a heading color", async function () {
+      const color = await pup.getCSSProperty(page, "h2", "color");
 
-			expect(color).to.eql("rgb(255, 255, 255)");
-		});
+      expect(color).to.eql("rgb(255, 255, 255)");
+    });
 
-		it("should have a margin-top: 20px", async function () {
-			const marginTop = await pup.getCSSProperty(page, "h2", "marginTop");
+    it("should have a margin-top: 20px", async function () {
+      const marginTop = await pup.getCSSProperty(page, "h2", "marginTop");
 
-			expect(marginTop).to.eql("20px");
-		});
+      expect(marginTop).to.eql("20px");
+    });
 
 
-		it("should have a font-size: 64px", async function () {
-			const fontSize = await pup.getCSSProperty(page, "h2", "fontSize");
+    it("should have a font-size: 64px", async function () {
+      const fontSize = await pup.getCSSProperty(page, "h2", "fontSize");
 
-			expect(fontSize).to.eql("64px");
-		});
-		it("should have a heading", async function () {
-			const HEADING_SELECTOR = "h2";
-			const headingText = await pup.getInnerText(page, HEADING_SELECTOR);
-			expect(headingText).to.eql("Schedule");
-		});
-	});
+      expect(fontSize).to.eql("64px");
+    });
+    it("should have a heading", async function () {
+      const HEADING_SELECTOR = "h2";
+      const headingText = await pup.getInnerText(page, HEADING_SELECTOR);
+      expect(headingText).to.eql("Schedule");
+    });
+  });
 });
--- demo/index.js
+++ demo/index.js
@@ -1,23 +1,23 @@
 const hapi = require('hapi');
 const server = hapi.server({
-	port: 4000,
-	host: 'localhost'
+  port: 4000,
+  host: 'localhost'
 });
 
 const init = async () => {
 
-	server.route([
-		{
-			method: 'GET',
-			path: '/',
-			handler: (req, reply) => {
-				return "<h2>Schedule</h2>";
-			}
-		}
-	]);
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: (req, reply) => {
+        return "<h2>Schedule</h2>";
+      }
+    }
+  ]);
 
-	await server.start();
-	console.log('Server running at: ' + server.info.uri);
+  await server.start();
+  console.log('Server running at: ' + server.info.uri);
 };
 
 init();
