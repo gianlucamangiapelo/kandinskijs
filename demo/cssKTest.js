9bff940d5dca28019d90ebaf7193ba8341feedbb
Alberto Maghini <alberto.maghini@deltatre.com>
2019-04-24 18:34:10 +0200

    moved to serve package

--- demo/cssKTest.js
+++ demo/cssKTest.js
@@ -2,96 +2,100 @@ var expect = require("chai").expect;
 const kisk = require("../src/kandinski");
 const cssHelper = kisk.cssHelper;
 
-describe("h2 css test", function () {
-
-  const url = "http://localhost:4000";
+describe("h2 css test", function() {
+  const url = "http://localhost:5000";
   const cssPath = "demo/localBase.css";
 
-  before(async function () {
+  before(async function() {
     await kisk.init(url, cssPath);
   });
 
-  after(async function () {
+  after(async function() {
     await kisk.destroy();
   });
 
-  context("mobile viewport", function () {
-    before(async function () {
+  context("mobile viewport", function() {
+    before(async function() {
       await kisk.getPage({ width: 320, height: 1 });
     });
-    after(async function () {
+    after(async function() {
       await kisk.page.close();
     });
-    it("h2 should have a margin: auto", async function () {
+    it("h2 should have a margin: 0", async function() {
       const margin = await kisk.getCSSProperty("h2", "margin");
       expect(margin).to.eql("0px");
     });
-    it("h2 should have a text-align: center", async function () {
+    it("h2 should have a text-align: center", async function() {
       const margin = await kisk.getCSSProperty("h2", "text-align");
       expect(margin).to.eql("center");
     });
-    it("h2 should have a width: 33%", async function () {
+    it("h2 should have a width: 33%", async function() {
       const width = await kisk.getPctCSSProperty("h2", "width");
       expect(width).to.eql("33%");
     });
+
+    it("h2 should have a margin-bottom: 15%", async function() {
+      const marginBottom = await kisk.getPctCSSProperty("h2", "marginBottom");
+      expect(marginBottom).to.eql("15%");
+    });
   });
 
-  context("tablet viewport", function () {
-    before(async function () {
+  context("tablet viewport", function() {
+    before(async function() {
       await kisk.getPage({ width: 768, height: 1 });
     });
-    after(async function () {
+    after(async function() {
       await kisk.page.close();
     });
 
-    it("h2 should have a display: flex", async function () {
+    it("h2 should have a display: flex", async function() {
       const display = await kisk.getCSSProperty("h2", "display");
       expect(display).to.eql("flex");
     });
   });
 
-  context("desktop viewport", function () {
-    before(async function () {
+  context("desktop viewport", function() {
+    before(async function() {
       await kisk.getPage({ width: 1024, height: 1 });
     });
-    after(async function () {
+    after(async function() {
       await kisk.page.close();
     });
 
-    it("h2 should have a display: block", async function () {
+    it("h2 should have a display: block", async function() {
       const display = await kisk.getCSSProperty("h2", "display");
       expect(display).to.eql("block");
     });
 
-    it("should have a heading color rgb", async function () {
+    it("should have a heading color rgb", async function() {
       const color = await kisk.getCSSProperty("h2", "color");
       expect(color).to.eql("rgb(255, 255, 255)");
     });
 
-    it("should have a heading color hex", async function () {
+    it("should have a heading color hex", async function() {
       const color = await kisk.getCSSProperty("h2", "color");
       expect(cssHelper.rgbToHex(color)).to.eql("#ffffff");
     });
 
-    it("should have a margin-top: 20px", async function () {
+    it("should have a margin-top: 20px", async function() {
       const marginTop = await kisk.getCSSProperty("h2", "marginTop");
       expect(marginTop).to.eql("20px");
     });
 
-    it("should have a width: 20%", async function () {
+    it("should have a width: 20%", async function() {
       const widthPx = await kisk.getCSSProperty("h2", "width");
       const widthPerc = cssHelper.pxToPerc(widthPx, kisk.parentBoxModel.width);
       expect(widthPerc).to.eql("20%");
     });
 
-    it("should have a font-size: 64px", async function () {
+    it("should have a font-size: 64px", async function() {
       const fontSize = await kisk.getCSSProperty("h2", "fontSize");
       expect(fontSize).to.eql("64px");
     });
-    it("should have a heading text = Kandinskijs", async function () {
+    it("should have a heading text = What a wonderful test!", async function() {
       const HEADING_SELECTOR = "h2";
       const headingText = await kisk.getInnerText(HEADING_SELECTOR);
-      expect(headingText).to.eql("Kandinskijs");
+      expect(headingText).to.eql("What a wonderful test!");
     });
   });
 });
