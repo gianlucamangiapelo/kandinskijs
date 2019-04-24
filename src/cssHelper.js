fa1b687e9dc31f65cf2a0b661cb9d3dc36b89e40
Alberto Maghini <alberto.maghini@deltatre.com>
2019-04-24 18:33:10 +0200

    fixed pxToPerc when pxParent is undefined

--- src/cssHelper.js
+++ src/cssHelper.js
@@ -1,36 +1,43 @@
+const debug = require("debug");
+const dbg = debug("kandinskijs:cssHelper");
 module.exports = {
-  hexToRgb: function (hex) {
-    const hs = hex[0] === '#' ? hex.slice(1) : hex;
+  hexToRgb: function(hex) {
+    const hs = hex[0] === "#" ? hex.slice(1) : hex;
     return [
       parseInt(hs[0] + hs[1], 16),
       parseInt(hs[2] + hs[3], 16),
       parseInt(hs[4] + hs[5], 16)
     ];
   },
-  rgbToHex: function (rgbString) {
-    let rgbArray = rgbStringToArray(rgbString);
-    return rgbArray.reduce((hex, c) => hex + padHex(c.toString(16)), '#');
+  rgbToHex: function(rgbString) {
+    const rgbArray = rgbStringToArray(rgbString);
+    return rgbArray.reduce((hex, c) => hex + padHex(c.toString(16)), "#");
   },
-  pxToPerc: function (pxSelector, pxParent) {
+  pxToPerc: function(pxSelector, pxParent) {
+    if (!pxParent) {
+      return -Infinity;
+    }
     const intPxSelector = parsePx(pxSelector);
-    return Math.ceil((parseFloat(intPxSelector / pxParent) * 100)).toString().concat("%");
+    if (isNaN(intPxSelector)) {
+      return -Infinity;
+    }
+    return Math.ceil(parseFloat(intPxSelector / pxParent) * 100)
+      .toString()
+      .concat("%");
   }
 };
 
-const parsePx = function (pxString) {
+const parsePx = function(pxString) {
   const regex = /[0-9]+/gm;
-  let match = regex.exec(pxString);
-  if (match != null) {
-    return match;
-  }
-}
+  return regex.exec(pxString)[0];
+};
 
-const rgbStringToArray = function (rgbString) {
-  if (typeof (rgbString) != "string") {
+const rgbStringToArray = function(rgbString) {
+  if (typeof rgbString != "string") {
     throw new Error("rgbString is not a string");
   }
 
-  let rgbArray = [];
+  const rgbArray = [];
   let matches;
   const regex = /rgb\(([0-9]{1,3}),([0-9]{1,3}),([0-9]{1,3})\)/gm;
 
@@ -46,6 +53,6 @@ const rgbStringToArray = function (rgbString) {
     }
   }
   return rgbArray;
-}
+};
 
-const padHex = hexStr => hexStr.length === 1 ? `0${hexStr}` : hexStr;
+const padHex = hexStr => (hexStr.length === 1 ? `0${hexStr}` : hexStr);
