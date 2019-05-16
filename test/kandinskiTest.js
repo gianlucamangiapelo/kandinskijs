694dc5b58939d4e8c14ddb41240f1511a4edad7e
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2019-05-16 21:26:32 +0200

    Update test suite with new kandinskijs plugin updates

--- test/kandinskiTest.js
+++ test/kandinskiTest.js
@@ -2,8 +2,8 @@ var expect = require("chai").expect;
 const kisk = require("../src/kandinski");
 const sinon = require("sinon");
 
-describe("kandinskijs", function () {
-  it("should exist", function () {
+describe("kandinskijs", function() {
+  it("should exist", function() {
     expect(kisk).to.be.not.null;
   });
 
@@ -11,20 +11,20 @@ describe("kandinskijs", function () {
   //
   //Properties declaration
   //
-  context("module properties", function () {
-    it("should have browser property", function () {
+  context("module properties", function() {
+    it("should have browser property", function() {
       const browser = "browser";
       expect(kisk).to.have.property(browser);
     });
-    it("should have page property", function () {
+    it("should have page property", function() {
       const page = "page";
       expect(kisk).to.have.property(page);
     });
-    it("should have url property", function () {
+    it("should have url property", function() {
       const url = "url";
       expect(kisk).to.have.property(url);
     });
-    it("should have cssPath property", function () {
+    it("should have cssPath property", function() {
       const cssPath = "cssPath";
       expect(kisk).to.have.property(cssPath);
     });
@@ -34,33 +34,33 @@ describe("kandinskijs", function () {
   //
   //Methods declaration
   //
-  context("module methods", function () {
-    it("should have init method", function () {
+  context("module methods", function() {
+    it("should have init method", function() {
       const init = "init";
       expect(kisk).to.have.ownProperty(init);
       expect(kisk.init).to.be.a("function");
     });
-    it("should have destroy method", function () {
+    it("should have destroy method", function() {
       const destroy = "destroy";
       expect(kisk).to.have.ownProperty(destroy);
       expect(kisk.destroy).to.be.a("function");
     });
-    it("should have getPage method", function () {
+    it("should have getPage method", function() {
       const getPage = "getPage";
       expect(kisk).to.have.ownProperty(getPage);
       expect(kisk.getPage).to.be.a("function");
     });
-    it("should have getInnerText method", function () {
+    it("should have getInnerText method", function() {
       const getInnerText = "getInnerText";
       expect(kisk).to.have.ownProperty(getInnerText);
       expect(kisk.getInnerText).to.be.a("function");
     });
-    it("should have getCSSProperty method", function () {
+    it("should have getCSSProperty method", function() {
       const getCSSProperty = "getCSSProperty";
       expect(kisk).to.have.ownProperty(getCSSProperty);
       expect(kisk.getCSSProperty).to.be.a("function");
     });
-    it("should have getPctCSSProperty method", function () {
+    it("should have getPctCSSProperty method", function() {
       const getCSSProperty = "getPctCSSProperty";
       expect(kisk).to.have.ownProperty(getCSSProperty);
       expect(kisk.getCSSProperty).to.be.a("function");
@@ -71,8 +71,8 @@ describe("kandinskijs", function () {
   //
   //Helpers declaration
   //
-  context("module helpers", function () {
-    it("should have cssHelper", function () {
+  context("module helpers", function() {
+    it("should have cssHelper", function() {
       const cssHelper = "cssHelper";
       expect(kisk).to.have.property(cssHelper);
     });
@@ -82,38 +82,38 @@ describe("kandinskijs", function () {
   //
   //init()
   //
-  context("init()", function () {
-    afterEach(function () {
+  context("init()", function() {
+    afterEach(function() {
       kisk.destroy();
     });
-    it("should initialize browser", function (done) {
-      kisk.init("http://website.com", "path/file.css").then(function () {
+    it("should initialize browser", function(done) {
+      kisk.init(this, "http://website.com", "path/file.css").then(function() {
         expect(kisk.browser).not.to.be.undefined;
         done();
       });
     });
-    it("should initialize cssPath", function (done) {
-      kisk.init("http://website.com", "path/file.css").then(function () {
+    it("should initialize cssPath", function(done) {
+      kisk.init(this, "http://website.com", "path/file.css").then(function() {
         expect(kisk.cssPath).not.to.be.undefined;
         expect(kisk.cssPath).equal("path/file.css");
         done();
       });
     });
-    it("should initialize cssPath if cssPath parameter is null", function (done) {
-      kisk.init("http://website.com", null).then(function () {
+    it("should initialize cssPath if cssPath parameter is null", function(done) {
+      kisk.init(this, "http://website.com", null).then(function() {
         expect(kisk.cssPath).to.be.null;
         done();
       });
     });
-    it("should initialize url", function (done) {
-      kisk.init("http://website.com", "path/file.css").then(function () {
+    it("should initialize url", function(done) {
+      kisk.init(this, "http://website.com", "path/file.css").then(function() {
         expect(kisk.url).not.to.be.undefined;
         expect(kisk.url).equal("http://website.com");
         done();
       });
     });
-    it("should throw error if url is null", function (done) {
-      kisk.init(null, "path/file.css").catch(function (error) {
+    it("should throw error if url is null", function(done) {
+      kisk.init(this, null, "path/file.css").catch(function(error) {
         expect(error).to.be.not.undefined;
         done();
       });
@@ -125,23 +125,23 @@ describe("kandinskijs", function () {
   //
   //getPage()
   //
-  context("getPage", function () {
-    afterEach(function () {
+  context("getPage", function() {
+    afterEach(function() {
       kisk.destroy();
     });
 
-    it("should throw error because kisk has not been initialized", function (done) {
+    it("should throw error because kisk has not been initialized", function(done) {
       reset(kisk);
-      kisk.getPage({}).catch(function (error) {
+      kisk.getPage({}).catch(function(error) {
         expect(error).to.be.not.undefined;
         expect(error.message).to.be.equal("browser is not initialized");
         done();
       });
     });
 
-    it("should throw error because viewport has not been passed", function (done) {
-      kisk.init("http://website.com").then(function () {
-        kisk.getPage().catch(function (error) {
+    it("should throw error because viewport has not been passed", function(done) {
+      kisk.init(this, "http://website.com").then(function() {
+        kisk.getPage().catch(function(error) {
           expect(error).to.be.not.undefined;
           expect(error.message).to.be.equal("viewport has not been defined");
           done();
@@ -149,10 +149,10 @@ describe("kandinskijs", function () {
       });
     });
 
-    it("should throw error because viewport.width has not been defined", function (done) {
+    it("should throw error because viewport.width has not been defined", function(done) {
       const viewport = {};
-      kisk.init("http://website.com").then(function () {
-        kisk.getPage(viewport).catch(function (error) {
+      kisk.init(this, "http://website.com").then(function() {
+        kisk.getPage(viewport).catch(function(error) {
           expect(error).to.be.not.undefined;
           expect(error.message).to.be.equal("viewport.width has not been defined");
           done();
@@ -160,10 +160,10 @@ describe("kandinskijs", function () {
       });
     });
 
-    it("should throw error because viewport.height has not been defined", function (done) {
+    it("should throw error because viewport.height has not been defined", function(done) {
       const viewport = { width: 320 };
-      kisk.init("http://website.com").then(function () {
-        kisk.getPage(viewport).catch(function (error) {
+      kisk.init(this, "http://website.com").then(function() {
+        kisk.getPage(viewport).catch(function(error) {
           expect(error).to.be.not.undefined;
           expect(error.message).to.be.equal("viewport.height has not been defined");
           done();
@@ -173,7 +173,7 @@ describe("kandinskijs", function () {
   });
 });
 
-const reset = function (kisk) {
+const reset = function(kisk) {
   kisk.browser = undefined;
   kisk.page = undefined;
   kisk.url = undefined;
