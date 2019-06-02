66693b8e49a074c99b95d77c1fd3d0e554752513
Alberto Maghini <alberto.maghini@deltatre.com>
2019-06-02 15:19:02 +0200

    improved for-loop in reporter

--- src/reporter/reporter.js
+++ src/reporter/reporter.js
@@ -28,8 +28,6 @@ module.exports = function(opts) {
       const _mappings = loadMappings();
       const { cssPath, maps } = _mappings;
       const result = {};
-      let totalRules = 0;
-      let testedRules = 0;
       const code = fs.readFileSync(cssPath, "utf8");
       const lines = {};
       const stmtMaps = {};
@@ -44,9 +42,8 @@ module.exports = function(opts) {
             if (!(elmMapping && elmMapping.props)) {
               continue;
             }
-            totalRules += elmMapping.props.length;
-            testedRules += elmMapping.props.filter(p => p.hit).length;
-            for (let i = 0; i < elmMapping.props.length; i++) {
+            const propsLength = elmMapping.props.length;
+            for (let i = propsLength - 1; i >= 0; i--) {
               const prop = elmMapping.props[i];
               lines[prop.range.startLine + 1] =
                 lines[prop.range.startLine + 1] || 0;
