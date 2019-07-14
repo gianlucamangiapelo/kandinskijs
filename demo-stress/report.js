66a57acd4bf53570fa535570d0c0cf78103d73aa
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2019-07-14 12:12:17 +0200

    Update: with single report generator

--- demo-stress/report.js
+++ demo-stress/report.js
@@ -1,5 +1,6 @@
 const kjsReporter = require("../src/reporter/reporter");
 const reporter = new kjsReporter({
-  outDir: "__logs__/css-stressKTest/"
+  outDir: "__logs__/css-stressKTest/",
+  singleDir: true
 });
 reporter.writeReport();
